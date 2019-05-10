2412a6c6d7417e5f76c6166fd3ed1883e795aaba
LeeChangYoon <41320453+LeeChangYoon@users.noreply.github.com>
2019-05-10 10:41:27 +0900

    주석을 달고 앞으로 일정하게 가는 코드 작성
    일단 전체적인 코드를 다듬었음

--- sketch_may10a.ino
+++ sketch_may10a.ino
@@ -1,14 +1,14 @@
 int L_EN = PIN_D0;
-int L_DIR_A = PIN_D1; // 00, 01, 10, 11 --> 01, 10 일때만 굴러간다. 00 바로 멈춤, 11 감속하면서 정지
-int L_DIR_B = PIN_D2; // 00, 01, 10, 11 --> 01, 10 일때만 굴러간다. 00 바로 멈춤, 11 감속하면서 정지
+int L_DIR_A = PIN_D1; // Forward --> Left Motor
+int L_DIR_B = PIN_D2; // Backward --> Left Motor
 int R_EN = PIN_B7;
-int R_DIR_A = PIN_B2; // 00, 01, 10, 11 --> 01, 10 일때만 굴러간다. 00 바로 멈춤, 11 감속하면서 정지
-int R_DIR_B = PIN_B3; // 00, 01, 10, 11 --> 01, 10 일때만 굴러간다. 00 바로 멈춤, 11 감속하면서 정지
+int R_DIR_A = PIN_B2; // Forward --> Right Motor
+int R_DIR_B = PIN_B3; // Backward --> Right Motor
 
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(9600); 
   pinMode(PIN_D1, OUTPUT);
   pinMode(PIN_D2, OUTPUT);
   pinMode(PIN_B2, OUTPUT);
@@ -17,20 +17,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+
+  // Make it go forward
   digitalWrite(PIN_D0, HIGH);
   digitalWrite(PIN_D1, HIGH);
-  // digitalWrite(PIN_D2, HIGH);
-  /*
-  digitalWrite(PIN_D1, LOW);
-  digitalWrite(PIN_D2, LOW);
-  digitalWrite(PIN_D0, LOW);
 
-  
   digitalWrite(PIN_B7, HIGH);
   digitalWrite(PIN_B2, HIGH);
-  digitalWrite(PIN_B3, HIGH);
-  digitalWrite(PIN_B2, LOW);
-  digitalWrite(PIN_B3, LOW);
-  digitalWrite(PIN_B7, LOW);
-  */
 }
