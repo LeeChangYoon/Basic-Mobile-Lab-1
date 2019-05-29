31dbb8726799ed5871aaf90782d33d7af18ab124
LeeChangYoon <forbackup0405@gmail.com>
2019-05-29 21:14:48 +0900

    a

--- Basic_Mobile_Project/Sound.h
+++ Basic_Mobile_Project/Sound.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #define OC1A_WIDTH OCR1A
 #define OC1_PERIOD ICR1
 
@@ -10,3 +11,17 @@ uint16_t frequency_return(uint16_t fre);
 void tone(uint8_t scale, uint16_t delay);
 void delay_ms(int ms);
 
+=======
+#define OC1A_WIDTH OCR1A
+#define OC1_PERIOD ICR1
+
+enum tone_scale
+{
+	No, Do, Rae, Mi, Pa, Sol, Ra, Si, Do_5
+};
+
+uint16_t frequency_return(uint16_t fre);
+void tone(uint8_t scale, uint16_t delay);
+void delay_ms(int ms);
+
+>>>>>>> parent of 8962eb5... Add files via upload
